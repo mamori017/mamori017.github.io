97334fe07d963153179aa86b18709c185a597cb6
mamori017 <mamori017@outlook.jp>
2018-05-10 09:21:32 +0900

    For now

--- js/default.js
+++ js/default.js
@@ -1,19 +1,23 @@
 
 $(function () {
+  var strTitle = 'mamori017.github.io'
+  var apiUrl = 'https://api.github.com/users/mamori017'
+  var dt = new Date();
+
   // Title
   var title = new Vue({
     el:'#title',
-      data:{text:'mamori017.github.io'}
+      data:{text:strTitle}
   })
 
   // Navbar title
   var navtitle = new Vue({
     el:'#nav-title',
-      data:{text:'mamori017.github.io'}
+      data:{text:strTitle}
   })
 
   // Get GitHub user info
-  $.get('https://api.github.com/users/mamori017').then(function (lists) {
+  $.get(apiUrl).then(function (lists) {
     user.lists = lists;
   });
 
@@ -26,7 +30,7 @@ $(function () {
   });
 
   // Get GitHub repositories info
-  $.get('https://api.github.com/users/mamori017/repos').then(function (lists) {
+  $.get(apiUrl + '/repos').then(function (lists) {
     repo.lists = lists;
   });
 
@@ -37,4 +41,27 @@ $(function () {
       lists:[]
     }
   });
+
+  // Footer
+  /*
+  Vue.component('elem', {
+    template: '<p class="text-muted text-center">&copy;&nbsp;'+ dt.getFullYear() + '&nbsp;mamori017</p>'
+  })
+  var footer = new Vue({
+    el: '#footer',
+  })
+  */
+  var extendElem = Vue.extend({
+    template: '<p class="text-muted text-center">&copy;&nbsp;'+ dt.getFullYear() + '&nbsp;mamori017</p>'
+  })
+  Vue.component('elem', extendElem)
+  var footer = new Vue({
+    el: '#footer'
+  })
+
+  Vue.nextTick(function() {
+    console.log('nextTick');
+  })
+
 });
+
